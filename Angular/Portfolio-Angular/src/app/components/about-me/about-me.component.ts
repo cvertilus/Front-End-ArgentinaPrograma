3ce4a4c9b2374f12f66f7ba465aa9a18f639ae30
Colby <vertiluscolby@gmail.com>
2023-01-26 08:42:40 -0300

    empeze aagregar los buttones

--- Angular/Portfolio-Angular/src/app/components/about-me/about-me.component.ts
+++ Angular/Portfolio-Angular/src/app/components/about-me/about-me.component.ts
@@ -6,14 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
-eliminarTitle() {
-this.title = ""
-}
-test() {
-alert("funciono");
-}
-  title:string ="Acerca de mi";
+  
+  editarAboutMe(text: string) {
+    throw new Error('Method not implemented.');
+  }
+  eliminarAboutMe() {
+    this.aboutMe = ""
+  }
+
+  test() {
+    alert("funciono");
+  }
+  title: string = "Acerca de mi";
   aboutMe: string = "Lorem ipsumdolor sit amet consectetur adipisicingelit. Repellat saepe necessitatibus sequi sed exercitationem velit earum. Repudiandae, nihil at,accusantium corporis dicta fugit exercitationem doloremque voluptate sapiente eligendi rerumveritatis"
-  profile:string = "assets/img/Luffy.jpg";
+  profile: string = "assets/img/Luffy.jpg";
 
 }
