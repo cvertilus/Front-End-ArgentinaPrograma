4d67e99ada47803e5f2b4c52e465603b325c784c
cvertilus <47528588+cvertilus@users.noreply.github.com>
2023-04-29 15:13:40 -0300

    Delete Bootstrap directory

--- Bootstrap/EpisNeges/java.js
+++
@@ -1,8 +0,0 @@
-let searchbtn = document.querySelector("#search-btn");
-let searchbar = document.querySelector('.search-bar');
-
-searchbtn.addEventListener('click', () =>{
-    searchbtn.classList.toggle('fa-times');
-    searchbar.classList.toggle('active');
-    
-});
